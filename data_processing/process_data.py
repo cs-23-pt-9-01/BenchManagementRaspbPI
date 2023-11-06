33d29b99775ef1088c064d5190ab0198a55879f8
Simon <sabj17@student.aau.dk>
2023-11-06 10:17:12 +0100

    Create process_data.py
    Update as helper functions are implemented

---
+++ data_processing/process_data.py
@@ -0,0 +1,12 @@
+
+
+def process_data(list_rapl_files, powerplug_data):
+    #try:
+    #check_integrity(list_rapl_files, powerplug_data)
+    #except:
+    #    DataIntegrityError
+
+    #merge_data(list_rapl_files, powerplug_data)
+
+    #Other data stuff
+    pass
