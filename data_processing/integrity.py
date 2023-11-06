61176f01046bc20973684b74ecdd09d6fd306aa9
Villiam <Vjacob19@student.aau.dk>
2023-11-06 12:42:37 +0100

    python library for checking dataframes

---
+++ data_processing/integrity.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+def checkLen(df, min):
+    if len(df) == 0:
+        raise Exception("Empty dataframe")
+    if len(df) < min:
+        raise Exception("Dataframe too short")
+
+
+def checkTypes(df, types):
+    for i in range(len(df.columns)):
+        if df.dtypes[i] != types[i]:
+            raise Exception("Invalid type: " + df.columns[i] + " expected: " + str(types[i]))
+
+# Check if each column exists in the dataframe
+def checkColumns(df, columns):
+    df_columns = df.columns
+    for i in range(len(df_columns)):
+        if not df_columns[i] == columns[i]:
+            raise Exception("invalid column, expected: " + columns[i] + " got: " + df_columns[i])
+
+# Validates the integrity of a dataframe
+# df : dataframe to be validated
+# min_len : minimum length of rows
+# columns : expected columns (in order)
+# types : expected types (in order)
+# The default values are from the RAPL interface measurements
+def checkIntegrity(
+        df, 
+        min_len = 1, 
+        columns = [
+            "PP0Start" ,
+            "PP0End" ,
+            "PP1Start", 
+            "PP1End", 
+            "PkgStart", 
+            "PkgEnd", 
+            "DramStart", 
+            "DramEnd"
+        ], 
+        types = [
+            "int64",
+            "int64",
+            "int64",
+            "int64",
+            "int64",
+            "int64",
+            "int64",
+            "int64"
+        ]):
+    checkLen(df, min_len)
+    checkColumns(df, columns )
+    checkTypes(df, types)
+
+# Check integrity of multiple files using checkIntegrity
+def checkMultipleFiles(files):
+    correct_files = []
+    for file in files:
+        try:
+            df = pd.read_csv(file)
+            checkIntegrity(df)
+            correct_files.append(file)
+        except Exception as e:
+            print("Error in " + file + " : " + str(e))
+    return correct_files
+
+if __name__ == '__main__':
+    raise Exception("This is a library, not a script")
+
