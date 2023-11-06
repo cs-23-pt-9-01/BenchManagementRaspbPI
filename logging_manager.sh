5cae167b4fa19573f31b64d9fb3fa8b259a23fe3
Simon <sabj17@student.aau.dk>
2023-11-06 14:48:16 +0100

    Update logging_manager.sh
    fixed path for loggingmanager

--- logging_manager.sh
+++ logging_manager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $1 == 1 ]; then
-  python kasa_energy_consumption.py
+  python ./BenchManagementRaspbPI/kasa_energy_consumption.py
 
 elif [ $1 == 0 ]; then
-  python temp_socket_testing_manager.py
+  python ./BenchManagementRaspbPI/temp_socket_testing_manager.py
 
 else
   echo $1 is an invalid argument. Valid arguments
