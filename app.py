a80bb7100b454e755891f9c9665baa41bd69d867
Villiam <Vjacob19@student.aau.dk>
2023-11-07 11:21:40 +0100

    removed unused function and added some comments

--- app.py
+++ app.py
@@ -49,34 +49,37 @@ async def start_runner():
     # finding devices and starting measurement
     devices = await kec.find_devices()
 
-    # starting asyncio loop
+    # starting asyncio loop be used by measurement_loop
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     measurement_loop(loop, stop_flag, devices, 1)
 
-    #threading.Thread(target=runMeasurement, args=(stop_flag,devices)).start()
-    
-# function for running measurement with asyncio on a thread
-def runMeasurement(stop_flag,devices):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(kec.thread_measurement(stop_flag,devices))
-    loop.close()
-
-# function for running a measurement periodically
-# inpsired by https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
 def measurement_loop(loop,stop_flag, devices, sec):
-    
+    """
+    function for running a measurement periodically
+    inpsired by https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
+
+    :param loop is a asyncio event loop, which is used for enabling threading and async io.
+    :param stop_flag is a threading event, which is used to stop
+    :param devices are the devices from kec.find_devices()
+    :param  sec is the time between measurements in seconds (only secounds because of powerplug)
+    """
+
+    # function wrapper to be called by timer
     def func_wrapper():
-        # stopping is flag is set
+        # stopping if flag is set
         if stop_flag.is_set():
             loop.close()
             return
         
+        # starting timer for next measurement
         measurement_loop(loop, stop_flag, devices, sec)
+
+        # running measurement
         loop.run_until_complete(kec.measure(devices))
-        
+    
+    # starting timer, that calls func_wrapper for measurement
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
@@ -89,4 +92,4 @@ def stop_runner():
     running = False
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host="192.168.0.5") # change ip to device ip
--- kasa_energy_consumption.py
+++ kasa_energy_consumption.py
@@ -64,43 +64,6 @@ async def find_devices():
         raise Exception("No monitoring devices found.")
     
     return energy_devices
-
-# Measurement function, flag is a threading.Event() object, energy_devices is from find_devices()
-async def thread_measurement(flag, energy_devices):
-    # Create or open a CSV file for writing
-    with open("energy_measurements.csv", "w", newline='') as csvfile:
-        fieldnames = ["timestamp", "device_alias", "address", "power_W", "voltage_V", "current_A"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # Write the header to the CSV
-        writer.writeheader()
-
-        while not flag.is_set():
-            for addr, device in energy_devices.items():
-                # Update device state (this also fetches the latest energy readings)
-                await device.update()
-
-                # Retrieve energy metrics
-                energy_info = device.emeter_realtime
-
-                # Get current timestamp with milliseconds
-                current_time = time.time()
-                
-                # Write energy metrics along with the timestamp to CSV
-                writer.writerow({
-                    "timestamp": current_time,
-                    "device_alias": device.alias,
-                    "address": addr,
-                    "power_W": energy_info['power'],
-                    "voltage_V": energy_info['voltage'],
-                    "current_A": energy_info['current']
-                })
-
-                # Optional: print to console
-                print(f"[{current_time}] {device.alias} at {addr} - Power: {energy_info['power']} W, Voltage: {energy_info['voltage']} V, Current: {energy_info['current']} A")
-
-            # Wait for a few seconds before fetching again
-            await asyncio.sleep(1)
     
 # function for taking a single for all energy_devices measurement and writing it to file
 async def measure(energy_devices):
