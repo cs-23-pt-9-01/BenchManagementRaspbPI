b054a7aeb762611e0b22782c0da2730328f9ede4
Villiam <Vjacob19@student.aau.dk>
2023-11-07 10:14:21 +0100

    too many params and missing stop flag check

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ async def start_runner():
     # finding devices and starting measurement
     devices = await kec.find_devices()
     measurement_loop(stop_flag, devices, 1)
-    
+
     #threading.Thread(target=runMeasurement, args=(stop_flag,devices)).start()
     
 # function for running measurement with asyncio on a thread
@@ -60,14 +60,18 @@ def runMeasurement(stop_flag,devices):
     loop.close()
 
 # function for running a measurement periodically
-# https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
+# inpsired by https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
 def measurement_loop(stop_flag, devices, sec):
     
     def func_wrapper(stop_flag, devices, sec):
+        # stopping is flag is set
+        if stop_flag.is_set():
+            return
+        
         measurement_loop(stop_flag, devices, sec)
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(kec.measure(stop_flag,devices))
+        loop.run_until_complete(kec.measure(devices))
         loop.close()
 
     t = threading.Timer(sec, func_wrapper, [stop_flag, devices, sec])
